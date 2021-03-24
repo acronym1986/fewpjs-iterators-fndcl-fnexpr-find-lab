9bb10cc931b6dabb360e00906cb3db034dba2e2e
Tim <acronym1986@gmail.com>
2021-03-24 23:40:39 +1100

    code created and passed test

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 
+/*function superbowlWin(obj, key, value) {
+   return obj.find(function (a) {
+       return a[key] === value;
+   })
+}
+superbowlWin(record);  */
+
+
+/*
+function superbowlWin (arr) {
+    return arr.find(function (obj) {
+        if (obj === { year: '1969', result: 'W'}){
+            return obj.year;
+        }
+    } );
+}  
+ 
+function superbowlWin (obj){
+     let ind = obj.indexOf({ year: '1969', result: 'W'});
+
+}*/
+function superbowlWin (arr) {
+    
+    let a = arr.find( ({ year }) => year === '1969' || year === void 0);
+     
+    
+       return a.year;
+     
+        
+       
+} 
+
+ 
+              
+
+ 
